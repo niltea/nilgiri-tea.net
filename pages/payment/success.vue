564d97bc683263bf552757f0e7d50b869ccb892c
niltea <futurabodoni@gmail.com>
2021-01-28 13:52:44 +0900

    支払完了ページ

--- pages/payment/success.vue
+++ pages/payment/success.vue
@@ -1,10 +1,20 @@
 <template lang="pug">
   .page-wrapper
     PageHeader
-    .payment
-      h2 Nilgiri Tea 主催各イベント参加費のお支払い
-      .lead
-        |お支払いが完了しました。
+    .form
+      h3 お支払い完了
+      .headLead
+        |お支払い手続きありがとうございました。
+        br
+        |準備会にて確認後、お支払い確認のご連絡をいたします。
+      .headLead
+        |一週間程度経ってもご連絡のない場合、
+        br
+        |お手数ではございますが、準備会へお問い合わせください。
+      .headLead
+        |また、サークルカットのご提出がお済みでない場合は、
+        br
+        |お早めの提出をお願いいたします。
 
 </template>
 
@@ -14,4 +24,26 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+.form {
+  position: relative;
+  border-radius: 15px;
+  box-shadow: 0 0 20px rgba(#000, .2);
+  box-sizing: border-box;
+  margin: 100px auto 10px;
+  padding: 40px;
+  overflow: hidden;
+  max-width: 900px;
+}
+
+h3 {
+  font-size: 28px;
+  text-align: center;
+}
+.headLead {
+  margin-top: 1em;
+  font-size: 16px;
+  text-align: center;
+  line-height: 2em;
+}
 </style>
